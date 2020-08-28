bb5ea05391c79c0b94130d408051ad0a472a1796
alexander.b <gituser1056@gmail.com>
2020-08-28 23:41:41 +0300

    CORS Fix2

--- app.js
+++ app.js
@@ -43,12 +43,12 @@ const allowedCors = [
   // 'https://praktikum.tk',
   // 'http://praktikum.tk',
   'localhost:8080',
-  'http://localhost:8080/',
+  'http://localhost:8080',
 ];
 
 app.use((req, res, next) => {
   const { origin } = req.headers; // Записываем в переменную origin соответствующий заголовок
-  console.log(origin);
+  // console.log(req.headers);
   if (allowedCors.includes(origin)) {
     // Проверяем, что значение origin есть среди разрешённых доменов
     res.header('Access-Control-Allow-Origin', origin);
