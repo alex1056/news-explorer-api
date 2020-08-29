7cfd619c2daf58eb8ab1ade30a39034bac34f264
alexander.b <gituser1056@gmail.com>
2020-08-29 15:05:08 +0300

    В Allow-Methods добавили OPTIONS

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ mongoose.connect(`mongodb://${MONGO_HOST}:${MONGO_PORT}/${DB_NAME}`, {
 app.use((req, res, next) => {
   // res.header('Access-Control-Allow-Origin', 'https://praktikum.tk');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
