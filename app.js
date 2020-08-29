9f651f38251a76dd70be5a1d87a546d448da2fff
alexander.b <gituser1056@gmail.com>
2020-08-29 14:53:25 +0300

    Установили Alow-credentials = true

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.use((req, res, next) => {
   // res.header('Access-Control-Allow-Origin', 'https://praktikum.tk');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
