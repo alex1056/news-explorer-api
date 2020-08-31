25b184c27756514dde3e6669c32828068a4ca9dc
alexander.b <gituser1056@gmail.com>
2020-08-31 13:56:57 +0300

    corsOptions changed

--- app.js
+++ app.js
@@ -69,9 +69,11 @@ app.use((req, res, next) => {
 const corsOptions = {
   origin: 'http://localhost:8080',
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  credentials: true,
+  methods: 'GET, POST, DELETE',
 };
 
-app.options('*', cors(corsOptions));
+// app.options('*', cors(corsOptions));
 // const whitelist = ['http://localhost:8080', 'http://example2.com']
 // const corsOptions = {
 //   origin = function (origin, callback) {
