358537de89c23c1ce723450098e19c8a633155a9
alexander.b <gituser1056@gmail.com>
2020-09-07 17:04:11 +0300

    SameSite -> None

--- controllers/users.js
+++ controllers/users.js
@@ -49,7 +49,8 @@ module.exports.login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
-          SameSite: 'None; Secure',
+          sameSite: 'none',
+          secure: true,
         })
         .end();
     })
